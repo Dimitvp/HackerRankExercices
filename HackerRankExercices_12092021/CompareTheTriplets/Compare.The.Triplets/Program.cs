7ffbd09c5b0296f06387dbf86a9fae2dea8d14be
Dimitar <dimitar.nikolov.tvp@gmail.com>
2021-09-12 12:03:06 +0300

    Add code from HackerRank

--- HackerRankExercices_12092021/CompareTheTriplets/Compare.The.Triplets/Program.cs
+++ HackerRankExercices_12092021/CompareTheTriplets/Compare.The.Triplets/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Compare.The.Triplets
 {
@@ -6,7 +9,37 @@ namespace Compare.The.Triplets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = Result.compareTriplets(a, b);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
         }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'compareTriplets' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY a
+         *  2. INTEGER_ARRAY b
+         */
+
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+
+        }
+
     }
 }
