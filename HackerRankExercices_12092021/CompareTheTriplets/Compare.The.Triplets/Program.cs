48f85fa5c6872c407d4b3a91fba240e14ffecc82
Dimitar <dimitar.nikolov.tvp@gmail.com>
2021-09-12 11:59:58 +0300

    Change folder structure

---
+++ HackerRankExercices_12092021/CompareTheTriplets/Compare.The.Triplets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compare.The.Triplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
