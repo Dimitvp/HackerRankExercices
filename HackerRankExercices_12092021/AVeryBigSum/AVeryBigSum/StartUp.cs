eca50e7e5196396b9c8233e4359ad9e810662a21
Dimitar <dimitar.nikolov.tvp@gmail.com>
2021-09-12 12:37:22 +0300

    Add project for the new problem

---
+++ HackerRankExercices_12092021/AVeryBigSum/AVeryBigSum/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AVeryBigSum
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+            long result = Result.AVeryBigSum(ar);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'aVeryBigSum' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+         */
+
+        public static long AVeryBigSum(List<long> ar)
+        {
+
+        }
+
+    }
+}
