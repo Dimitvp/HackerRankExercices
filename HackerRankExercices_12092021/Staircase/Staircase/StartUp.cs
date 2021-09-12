4032afdac2fcfc3726395521b3b978d430c1f534
Dimitar <dimitar.nikolov.tvp@gmail.com>
2021-09-12 15:17:19 +0300

    Add the new project Add the code from HackerRank

---
+++ HackerRankExercices_12092021/Staircase/Staircase/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Staircase
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.staircase(n);
+        }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'staircase' function below.
+         *
+         * The function accepts INTEGER n as parameter.
+         */
+
+        public static void staircase(int n)
+        {
+
+        }
+
+    }
+}
