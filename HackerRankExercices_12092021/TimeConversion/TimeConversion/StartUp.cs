8eb056f50b8982c535cbf4c14cc6eece86caf290
Dimitar <dimitar.nikolov.tvp@gmail.com>
2021-09-24 17:18:57 +0300

    Add solution for Time Conversion

---
+++ HackerRankExercices_12092021/TimeConversion/TimeConversion/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string result = Result.TimeConversion(s);
+
+            Console.WriteLine(result);
+        }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'timeConversion' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string TimeConversion(string s)
+        {
+            DateTime dt;
+            bool res = DateTime.TryParse(s, out dt);
+
+            return dt.ToString("HH:mm:ss");
+        }
+    }
+}
